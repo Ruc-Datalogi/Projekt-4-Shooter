bb72cbed931df39f9ed3ceb3d69f083e797efa35
Mylos97 <57527830+Mylos97@users.noreply.github.com>
2021-02-09 12:02:05 +0100

    hej

--- Game/Player.py
+++ Game/Player.py
@@ -1,1 +1,12 @@
 class Player:
+
+
+    def checkCollision(x,y):
+        if x <= 0:
+            x = 0
+        if x >= 584:
+            x = 584
+        if y <= 0: 
+            y = 0
+        if y >= 784:
+            y = 784
