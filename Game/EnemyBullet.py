20e8672a3a45b0a29274547fefdf915284a259b1
Christian Faber <cabf1995@gmail.com>
2021-02-24 09:24:53 +0100

    Updating enemy_bullet

--- Game/EnemyBullet.py
+++ Game/EnemyBullet.py
@@ -6,35 +6,35 @@ class EnemyBullet:
     
     def __init__(self, xPos, yPos):
         self.img = pygame.image.load("Game/sprites/bullets/enemy_bullets/green_bullet.png")
-        self.bulletX = xPos 
-        self.bulletY = yPos 
-        self.bulletSpeedX = 0
-        self.bulletSpeedY = 12
-        self.bulletRect = self.img.get_rect()
-        self.bulletDamage = 10
+        self.enemy_bulletX = xPos 
+        self.enemy_bulletY = yPos 
+        self.enemy_bulletSpeedX = 0
+        self.enemy_bulletSpeedY = 12
+        self.enemy_bulletRect = self.img.get_rect()
+        self.enemy_bulletDamage = 10
     
     ## For collision ##
     @property
     def getBulletRect(self):
-        return self.bulletRect
+        return self.enemy_bulletRect
     
     ## For damaging enemy ##
     @property
     def getBulletDamage(self):
-        return self.bulletDamage
+        return self.enemy_bulletDamage
 
     ## For removing the bullet ##
     @property  
     def getBulletY(self):
-        return self.bulletY
+        return self.enemy_bulletY
 
     ## Draw at specific location ##
     def bulletDraw(self , screen):        
-        screen.blit(self.img,(self.bulletX,self.bulletY))
+        screen.blit(self.img,(self.enemy_bulletX,self.enemy_bulletY))
 
     ## Update bullet pos and the rect ##
     def bulletMove(self):
-        self.bulletX -= self.bulletSpeedX
-        self.bulletY -= self.bulletSpeedY
-        self.bulletRect = self.img.get_rect(x=self.bulletX, y=self.bulletY)
+        self.enemy_bulletX += self.enemy_bulletSpeedX
+        self.enemy_bulletY += self.enemy_bulletSpeedY
+        self.enemy_bulletRect = self.img.get_rect(x=self.enemy_bulletX, y=self.enemy_bulletY)
     
