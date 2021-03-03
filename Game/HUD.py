9b39cb73acecb6cb976dcb7351a41589748c591f
Christian Faber <cabf1995@gmail.com>
2021-03-03 13:00:40 +0100

    HUD UPDATE

--- Game/HUD.py
+++ Game/HUD.py
@@ -39,10 +39,12 @@ class HUD:
     def draw_energy_HUD(self):
         #Energybar background
         pygame.draw.rect(self.screen,(68,77,85), pygame.Rect(self.screen_size[0]*0.02,self.screen_size[1]*0.955, self.screen_size[0]*0.735, self.screen_size[1]*0.03))
+        #Energybar pisgul
         pygame.draw.rect(self.screen,(255, 213, 0), pygame.Rect(self.screen_size[0]*0.02,self.screen_size[1]*0.955, self.screen_size[0]*0.735, self.screen_size[1]*0.03))
-        for 
-            pygame.draw.rect(self.screen,(52, 61, 82), pygame.Rect(self.screen_size[0]*0.89,self.screen_size[1]*0.915, self.screen_size[0]*0.09, self.screen_size[1]*0.07),width=2)
-    
+        #Energybar brackets
+        for i in range(0,10):
+            pygame.draw.rect(self.screen,(0, 0, 0), pygame.Rect(self.screen_size[0]*0.02+i*self.screen_size[0]*0.0734,self.screen_size[1]*0.955, self.screen_size[0]*0.0734, (self.screen_size[1]-1)*0.03), width=2)
+        
     def draw_HUD(self):
         self.draw_overlay_HUD()
         self.draw_energy_HUD()
