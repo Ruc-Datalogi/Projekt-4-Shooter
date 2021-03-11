bcb8acb80d220fe86dd3c0a0273c06f48e4fd95e
Ebus <ebus@live.dk>
2021-03-11 12:43:00 +0100

    refactoring

---
+++ Game/Generator.py
@@ -0,0 +1,16 @@
+import sys, pygame, random
+from Mediator import *
+
+class Generator:
+
+    def __init__ (self, screen, mediator):
+        self.screen = screen
+        self.mediator = mediator
+        self.level = level
+
+    def generate (self):
+        if level == 0:
+            ##existing code
+        elif level == 1:
+            
+
