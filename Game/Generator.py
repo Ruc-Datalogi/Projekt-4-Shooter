7033a7b73f8793ffd4ce6793b44506a88bef7596
Christian Faber <cabf1995@gmail.com>
2021-03-16 10:05:35 +0100

    Generator update

--- Game/Generator.py
+++ Game/Generator.py
@@ -14,27 +14,23 @@ class Generator:
         self.current_wave = 0
 
         self.timer = 0
-
-    def generate_wave_1(self):
+    def get_spawn_points(self, enemy_amount):
         spawn_list = []
         random_int = 0
 
         ##Making sure the enemies don't spawn on top of each other
-        while len(spawn_list) < 6:
+        while len(spawn_list) < enemy_amount:
             random_int = random.randint(1,14)
             if random_int not in spawn_list:
                 spawn_list.append(random_int)
-        for i in range(0,6):
-            self.mediator.all_game_objects.append(Enemy(spawn_list[i]*20, (random.randint(10, 30)), 1, 'enemy', self.mediator, self.screen))
+        return spawn_list
+
+    def generate_wave_1(self, amount):
+        enemy_list = self.get_spawn_points(amount)
+        for i in range(len(self.enemy_list)):
+            self.mediator.all_game_objects.append(Enemy(enemy_list[i]*20, (random.randint(10, 30)), 1, 'enemy', self.mediator, self.screen))
 
     def generate_wave_2(self):
-        spawn_list = []
-        random_int = 0
-        
-        while len(spawn_list) < 4:
-            random_int = random.randint(1,14)
-            if random_int not in spawn_list:
-                spawn_list.append(random_int)
         
         for i in range (0, 4):
             self.mediator.all_game_objects.append(Enemy(spawn_list[i]*20, (random.randint(10, 30)), 2, 'enemy', self.mediator, self.screen))
@@ -53,37 +49,35 @@ class Generator:
     def get_level(self):
         return self.level
 
+    def next_wave(self):
+        if self.timer > 360 and self.current_wave < self.max_amount_of_waves:
+            self.current_wave += 1
+            self.timer = 0
+            return True
+        return False
+
+    def next_level(self):
+        if self.current_wave == self.max_amount_of_waves and not self.check_for_enemy():
+            self.current_wave = 0
+            self.max_amount_of_waves += 1
+            self.level += 1
+            self.timer = 0
+
+
 
     def generate(self):
+        self.timer += 1
         if self.level == 1:
-            self.timer += 1
+            
 
-            if self.timer > 360 and self.current_wave < self.max_amount_of_waves:
+            if self.next_wave():
                 
-                self.current_wave += 1
-                self.generate_wave_1()
-                self.timer = 0
+                self.generate_wave_1(6)
         
-            if self.current_wave == self.max_amount_of_waves and not self.check_for_enemy():
-                self.current_wave = 0
-                self.max_amount_of_waves += 1
-                self.level += 1
-                self.timer = 0
+            self.next_level()
+                
         
         if self.level == 2:
-            self.timer += 1
-
-            if self.timer > 180 and self.current_wave < self.max_amount_of_waves:
-                
-                self.current_wave += 1
-                self.generate_wave_1()
-                self.timer = 0
-        
-            if self.current_wave == self.max_amount_of_waves and not self.check_for_enemy():
-                self.level += 1
-
-    
-
-    
-
-
+            if self.next_wave():
+                self.generate_wave_1(8)
+            self.next_level()      
