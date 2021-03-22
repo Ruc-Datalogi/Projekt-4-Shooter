a8a43217171bdf9a39b5e6b9ce2804ee822bbd52
Ebus <ebus@live.dk>
2021-03-22 20:34:32 +0100

    skrrt

--- Game/Soundplayer.py
+++ Game/Soundplayer.py
@@ -4,7 +4,9 @@ class Soundplayer:
 
     def __init__(self):
         pygame.mixer.init()
-        self.enemy_hit = pygame.mixer.Sound('sound/pew.wav')
+        self.enemy_hit = pygame.mixer.Sound('Game/sound/pew.wav')
+        self.enemy_hit.set_volume(0.2)
+
 
     def enemy_hit_sound(self):
         self.enemy_hit.play()
