8f21e55c8d44cc04aaa040bb74e2dc789ad5c4fc
Mylos97 <57527830+Mylos97@users.noreply.github.com>
2021-05-10 11:51:24 +0200

    enemy

--- Game/Enemy.py
+++ Game/Enemy.py
@@ -111,19 +111,13 @@ class Enemy(GameObject):
     def loop(self):
         self.enemy_move()
 
-        if self.collision('f_bullet',self.enemy_rect) and self.enemy_damage_cooldown > 6:
+        hit_count = self.collision('f_bullet',self.enemy_rect)
+        if hit_count >= 1:
             self.enemy_damage_cooldown = 0
             Soundplayer.enemy_hit_sound(Soundplayer())
-
             self.enemy_health -= float(Upgrades.get_level_bullet_damage(Upgrades,JsonLoader.get_bullet_damage(JsonLoader)))*hit_count
             self.showing_image = self.temp_image
-        #if self.collision('f_bullet',self.enemy_rect): #and self.enemy_damage_cooldown > 6:
-            #self.enemy_damage_cooldown = 0
-            #Soundplayer.enemy_hit_sound(Soundplayer())
 
-            #self.enemy_health -= 10
-            #self.showing_image = self.temp_image
-        
         if self.enemy_health < 0:
             self.mediator.all_game_objects.append(Collectables (self.enemy_x, self.enemy_y, self.screen, self.mediator, 'coin'))
             self.mediator.to_be_removed.append(self)
